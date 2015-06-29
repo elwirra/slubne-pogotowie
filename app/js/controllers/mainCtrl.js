9da0e21a6bb3b60b59ca781246dac133d8024f23
lutharien <rakowska.a@gmail.com>
2015-06-29 14:14:38 +0200

    some add company wrap up & home page styles

--- app/js/controllers/mainCtrl.js
+++ app/js/controllers/mainCtrl.js
@@ -7,4 +7,27 @@ app.controller('mainCtrl', function($scope, $http, $firebaseObject, $firebaseArr
  	$scope.categories = $firebaseArray(categories);
   	$scope.reviews = $firebaseArray(reviews);
 
+  // 	var reviewCategory = "";
+  // 	$scope.categories.forEach(function(reviewCategory) {
+  // 		console.log(reviewCategory);
+  //   	if($scope.categories.categoryId === $scope.reviews.categoryId) {
+		//     reviewCategory = $scope.categories.name;
+		//     console.log(reviewCategory);
+		// }
+  //   });
+  	var reviewCategory = "";
+    angular.forEach($scope.categories, function(key, value) {
+    	console.log($scope.categories);
+	    if (key == categoryId && value == 4) {
+	      reviewCategory = category.name;
+	      console.log(reviewCategory);
+	      console.log(category.name);
+	    } else {
+	    	console.log("Nie pasuje");
+	    }
+	  });
+
+    $scope.reviewCategory = reviewCategory;
+
 });
+

03411b6fafb2ebc6c10d36da85c60aafee41f68d
lutharien <rakowska.a@gmail.com>
2015-07-09 18:59:03 +0200

    add company form improved

--- app/js/ui.js
+++ app/js/ui.js
@@ -1,3 +1,60 @@
+var inputNumber = document.getElementById('price-u-value');
+
+html5Slider.noUiSlider.on('update', function( values, handle ) {
+
+	var value = values[handle];
+
+	if ( handle ) {
+		inputNumber.value = value;
+	} else {
+		select.value = Math.round(value);
+	}
+});
+
+select.addEventListener('change', function(){
+	html5Slider.noUiSlider.set([this.value, null]);
+});
+
+inputNumber.addEventListener('change', function(){
+	html5Slider.noUiSlider.set([null, this.value]);
+});
+
+
+var secInputNumber = document.getElementById('price-l-value');
+
+html5Slider.noUiSlider.on('update', function( values, handle ) {
+
+	var value = values[handle];
+
+	if ( handle ) {
+		secInputNumber.value = value;
+	} else {
+		select.value = Math.round(value);
+	}
+});
+
+select.addEventListener('change', function(){
+	html5Slider.noUiSlider.set([this.value, null]);
+});
+
+secInputNumber.addEventListener('change', function(){
+	html5Slider.noUiSlider.set([null, this.value]);
+});
+
+
+var html5Slider = document.getElementById('price');
+
+noUiSlider.create(html5Slider, {
+	start: [ 20, 3000 ],
+	connect: true,
+	range: {
+		'min': 0,
+		'max': 10000
+	}
+});
+
+	
+
 $(function() {
 
 	// SQUARE TILES 
